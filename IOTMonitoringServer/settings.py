a03e0eff1421aee41b89855281b7ea40c135cd35
jjmontenegrop <jj.montenegro@uniandes.edu.co>
2024-09-21 00:46:03 -0500

    EC2 address configuration in settings.py

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.87.212.66"]
+ALLOWED_HOSTS = ["localhost", "34.207.108.93"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "100.24.51.123",  # Dirección IP de la base de datos
+        "HOST": "3.87.135.107",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.224.120.181"
+MQTT_HOST = "98.82.184.155"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
